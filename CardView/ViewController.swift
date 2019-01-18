e5bec698dee6cefcf99d2ca04c2d55fb36101aee
Shantaram Kokate <shantaramkokate@mywayit.local>
2019-01-18 14:52:10 +0530

    Initial Commit

---
+++ CardView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardView
+//
+//  Created by Shantaram Kokate on 1/18/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
