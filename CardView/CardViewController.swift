bb7a7acbce43b0bac7c78eef55991593fbe9a3cd
Shantaram Kokate <shantaramkokate@MyWayIT.local>
2019-01-28 17:22:48 +0530

    first commit

---
+++ CardView/CardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardViewController.swift
+//  CardView
+//
+//  Created by Shantaram Kokate on 1/18/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CardView/CardViewHandler.swift
@@ -0,0 +1,13 @@
+//
+//  CardViewHandler.swift
+//  CardView
+//
+//  Created by Shantaram Kokate on 1/23/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class CardViewHandler: NSObject {
+
+}
